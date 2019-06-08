cb417fdeace9eec5504edc267422d08a0d17d36e
RedGalaxySoftware <onankman@gmail.com>
2019-06-08 10:59:06 +0200

    Fixed build not working due to a tag error

--- src/main/java/modernity/common/block/MDBlockTags.java
+++ src/main/java/modernity/common/block/MDBlockTags.java
@@ -6,14 +6,14 @@ import net.minecraft.tags.Tag;
 import net.minecraft.util.ResourceLocation;
 
 public class MDBlockTags {
-    public static final Tag<Block> LOG = tag( "log" );
     public static final Tag<Block> BLACKWOOD_LOG = tag( "log/blackwood_log" );
+    public static final Tag<Block> LOG = tag( "log" );
 
-    public static final Tag<Block> LEAVES = tag( "leaves" );
     public static final Tag<Block> BLACKWOOD_LEAVES = tag( "leaves/blackwood_leaves" );
+    public static final Tag<Block> LEAVES = tag( "leaves" );
 
-    public static final Tag<Block> ROCK = tag( "rock" );
     public static final Tag<Block> DARKROCK = tag( "rock/darkrock" );
+    public static final Tag<Block> ROCK = tag( "rock" );
 
     private static Tag<Block> tag( String id ) {
         return new BlockTags.Wrapper( new ResourceLocation( "modernity", id ) );
