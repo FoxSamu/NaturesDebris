82e7b8f8dcf47322de85713ec831db6a3b08ba35
RedGalaxySoftware <onankman@gmail.com>
2019-06-18 14:31:19 +0200

    Added the right drops to salt ore

--- src/main/java/modernity/common/block/base/BlockOre.java
+++ src/main/java/modernity/common/block/base/BlockOre.java
@@ -4,12 +4,19 @@
  * Do not redistribute.
  *
  * By  : RGSW
- * Date: 6 - 17 - 2019
+ * Date: 6 - 18 - 2019
  */
 
 package modernity.common.block.base;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import modernity.common.item.MDItems;
 
 public class BlockOre extends BlockBase {
 
@@ -28,6 +35,22 @@ public class BlockOre extends BlockBase {
 
         public Salt( String id, Properties properties ) {
             super( id, properties );
+        }
+
+        @Override
+        public void getDrops( IBlockState state, NonNullList<ItemStack> drops, World world, BlockPos pos, int fortune ) {
+            int nuggetAmount = world.rand.nextInt( Math.max( 6 - fortune, 1 ) * 2 ) == 0 ? world.rand.nextInt( 2 ) : 0;
+            int dustAmount = world.rand.nextInt( fortune + 1 ) + 1;
+            int crystalAmount = world.rand.nextInt( 100 ) < fortune + 1 ? 1 : 0;
+            if( nuggetAmount > 0 ) {
+                drops.add( new ItemStack( MDItems.SALT_NUGGET, nuggetAmount ) );
+            }
+            if( dustAmount > 0 ) {
+                drops.add( new ItemStack( MDItems.SALT_DUST, dustAmount ) );
+            }
+            if( crystalAmount > 0 ) {
+                drops.add( new ItemStack( this, crystalAmount ) );
+            }
         }
     }
 }
