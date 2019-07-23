97121f37c496fb2740b2dab744d3e8bfa2189e62
RedGalaxySoftware <onankman@gmail.com>
2019-07-23 09:22:17 +0200

    Some version info changed

--- src/main/java/modernity/MDInfo.java
+++ src/main/java/modernity/MDInfo.java
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2019 RedGalaxy & co.
+ * Copyright (c) 2019 RedGalaxy & contributors
  * Licensed under the Apache Licence v2.0.
  * Do not redistribute.
  *
  * By  : RGSW
- * Date: 7 - 9 - 2019
+ * Date: 7 - 23 - 2019
  */
 
 package modernity;
 
 public class MDInfo {
-    public static final String VERSION = "INDEV-0.0.0";
-    public static final String VERSION_NAME = "First Release";
+    public static final String VERSION = "INDEV-0.0.2";
+    public static final String VERSION_NAME = "Wiki Update";
 }
