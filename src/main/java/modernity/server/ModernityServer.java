27c407172c0bc984c76a5930db005a9d930df362
RedGalaxySoftware <onankman@gmail.com>
2019-10-15 10:15:02 +0200

    Documented `ModernityServer`

--- src/main/java/modernity/server/ModernityServer.java
+++ src/main/java/modernity/server/ModernityServer.java
@@ -3,6 +3,10 @@ package modernity.server;
 import modernity.common.Modernity;
 import net.minecraftforge.fml.LogicalSide;
 
+/**
+ * Dedicated server side proxy class of the Modernity. This class does nothing more than indicating we're on the
+ * dedicated server, all loading and initialization is handled by the {@linkplain Modernity common proxy class}.
+ */
 public class ModernityServer extends Modernity {
     @Override
     public LogicalSide side() {
