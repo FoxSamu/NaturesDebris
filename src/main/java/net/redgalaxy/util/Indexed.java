7872a51e54451f181e7d9496516bdb2d6f59f037
RedGalaxySoftware <onankman@gmail.com>
2020-01-24 18:00:10 +0100

    Added some naming utilities

---
+++ src/main/java/net/redgalaxy/util/Indexed.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020 RedGalaxy
+ * All rights reserved. Do not distribute.
+ *
+ * Date:   01 - 24 - 2020
+ * Author: rgsw
+ */
+
+package net.redgalaxy.util;
+
+@FunctionalInterface
+public interface Indexed {
+    int index();
+}
