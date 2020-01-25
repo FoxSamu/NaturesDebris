47c854631e7c1d04f0f3dfb6adbe24d8b5eaf515
RedGalaxySoftware <onankman@gmail.com>
2020-01-25 10:05:51 +0100

    ?

--- src/main/java/modernity/api/util/PrimeNumbers.java
+++ src/main/java/modernity/api/util/PrimeNumbers.java
@@ -2,13 +2,13 @@
  * Copyright (c) 2020 RedGalaxy
  * All rights reserved. Do not distribute.
  *
- * Date:   01 - 11 - 2020
+ * Date:   01 - 25 - 2020
  * Author: rgsw
  */
 
 package modernity.api.util;
 
-class PrimeNumbers {
+final class PrimeNumbers {
     static final int[] RANDOM_PRIMES = {
         3, 5, 7, 11,
         13, 17, 19, 23,
@@ -17,4 +17,7 @@ class PrimeNumbers {
         61, 67, 71, 73,
         79, 83, 89, 97
     };
+
+    private PrimeNumbers() {
+    }
 }
