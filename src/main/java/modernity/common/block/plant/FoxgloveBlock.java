3c0299f8569d6f177a975b48cb0dc8803a76083d
RedGalaxySoftware <onankman@gmail.com>
2020-02-06 22:19:05 +0100

    Creative players no longer get nauseated from foxglove

--- src/main/java/modernity/common/block/plant/FoxgloveBlock.java
+++ src/main/java/modernity/common/block/plant/FoxgloveBlock.java
@@ -13,6 +13,7 @@ import modernity.common.util.MDDamageSource;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.math.BlockPos;
@@ -25,15 +26,18 @@ public class FoxgloveBlock extends DoubleStandingPlantBlock {
 
     @Override
     public void onEntityCollision( BlockState state, World world, BlockPos pos, Entity entity ) {
-        if( ! entity.getType().isContained( MDEntityTags.FOXGLOVE_IMMUNE ) )
+        if( entity instanceof PlayerEntity && ( ( (PlayerEntity) entity ).isCreative() || entity.isSpectator() ) )
+            return;
+        if( ! entity.getType().isContained( MDEntityTags.FOXGLOVE_IMMUNE ) ) {
             entity.attackEntityFrom( MDDamageSource.FOXGLOVE, 1 );
-        if( entity instanceof LivingEntity ) {
-            ( (LivingEntity) entity ).addPotionEffect( new EffectInstance(
-                Effects.NAUSEA, 200, 2, false, false
-            ) );
-            ( (LivingEntity) entity ).addPotionEffect( new EffectInstance(
-                Effects.POISON, 200, 2, false, false
-            ) );
+            if( entity instanceof LivingEntity ) {
+                ( (LivingEntity) entity ).addPotionEffect( new EffectInstance(
+                    Effects.NAUSEA, 200, 2, false, false
+                ) );
+                ( (LivingEntity) entity ).addPotionEffect( new EffectInstance(
+                    Effects.POISON, 200, 2, false, false
+                ) );
+            }
         }
     }
 }
