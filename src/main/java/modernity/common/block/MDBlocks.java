5fe5ce14dea2725d9b5c1d135d484dfece40d55e
RedGalaxySoftware <onankman@gmail.com>
2020-02-06 22:55:09 +0100

    Milk eye -> Milky eye

--- src/main/java/modernity/common/block/MDBlocks.java
+++ src/main/java/modernity/common/block/MDBlocks.java
@@ -501,7 +501,7 @@ public final class MDBlocks {
     public static final FlyFlowerStalkBlock FLY_FLOWER_STALK = blockItem( "fly_flower_stalk", new FlyFlowerStalkBlock( strongPlant( MaterialColor.GRASS, 0.5 ).sound( SoundType.WET_GRASS ) ), MDItemGroup.PLANTS );
     public static final FlyFlowerBlock FLY_FLOWER = blockItem( "fly_flower", new FlyFlowerBlock( strongPlant( MaterialColor.GRASS, 0.5 ) ), MDItemGroup.PLANTS );
     public static final SimplePlantBlock MURK_FLOWERS = blockItem( "murk_flower", new SimplePlantBlock( strongPlant( MaterialColor.AIR, 0 ), SimplePlantBlock.MURK_FLOWERS_SHAPE ), MDItemGroup.PLANTS );
-    public static final SimplePlantBlock MILK_EYE = blockItem( "milk_eye", new SimplePlantBlock( strongPlant( MaterialColor.AIR, 0 ), SimplePlantBlock.MILK_EYE_SHAPE ), MDItemGroup.PLANTS );
+    public static final SimplePlantBlock MILKY_EYE = blockItem( "milky_eye", new SimplePlantBlock( strongPlant( MaterialColor.AIR, 0 ), SimplePlantBlock.MILK_EYE_SHAPE ), MDItemGroup.PLANTS );
     public static final SimplePlantBlock EGIUM = blockItem( "egium", new SimplePlantBlock( strongPlant( MaterialColor.AIR, 0 ), SimplePlantBlock.EGIUM_SHAPE ), MDItemGroup.PLANTS );
     public static final ShadeBlueBlock SHADE_BLUE = blockItem( "shade_blue", new ShadeBlueBlock( strongPlant( MaterialColor.AIR, 0.1 ).sound( MDSoundTypes.SHADE_BLUE ) ), MDItemGroup.PLANTS );
     public static final SimplePlantBlock BLUE_BULBFLOWER = blockItem( "blue_bulbflower", new SimplePlantBlock( strongPlant( MaterialColor.AIR, 0 ), SimplePlantBlock.BULBFLOWER_SHAPE ), MDItemGroup.PLANTS );
